a1a1761e79537161cd748aa9e16800d63668a94f
Prajwal Basnet <prajalbasnet69@gmail.com>
2025-12-05 10:16:28 +0545

    Try Catch blocks in javascript

--- index.js
+++ index.js
@@ -17,24 +17,46 @@
 
 //callback lai solve garne tarika: Promises
 
-function fetchData() {
-    console.log("Fetching data...");
-    return new Promise((resolve, reject) =>{
-    setTimeout(() => {
-        resolve("Data fetched  1");
-    }, 1000)
-    setTimeout(() => {
-        resolve("Data fetched 2");
-    }, 1000)
-})
-    }
-    fetchData()
-    .then((result) =>{
-        console.log(result);
-    }) 
-    .catch((error) =>{
-        console.log(error);
-    }) 
-    .finally(() => {
-        console.log("Fetch attempt finished.");
-    })
+// function fetchData() {
+//     console.log("Fetching data...");
+//     return new Promise((resolve, reject) =>{
+//     setTimeout(() => {
+//         resolve("Data fetched  1");
+//     }, 1000)
+//     setTimeout(() => {
+//         resolve("Data fetched 2");
+//     }, 1000)
+// })
+//     }
+//     fetchData()
+//     .then((result) =>{
+//         console.log(result);
+//     }) 
+//     .catch((error) =>{
+//         console.log(error);
+//     }) 
+//     .finally(() => {
+//         console.log("Fetch attempt finished.");
+//     })
+
+
+//async await
+// function fetchData() {
+//     console.log("Fetching data...");
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => { resolve("Data fetched 1"); }, 1000)
+//     });
+// }
+
+// async function showData() {
+//     try { 
+//         let result = await fetchData();   // fetchData को Promise settle नभएसम्म पर्खन्छ
+//         console.log(result);
+//     } catch (error) {
+//         console.log(error);               // Error आएमा catch block मा आउनेछ
+//     } finally {
+//         console.log("Fetch attempt finished.");  // Promise settled भए/नभए पनि finally चल्छ
+//     }
+// }
+
+// showData();
---
+++ trycatch.js
@@ -0,0 +1,33 @@
+
+try {
+setTimeout(() => {
+    try {
+        console.log(prajal);
+    } catch (error) {
+       console.log("Error aayo haiii"); 
+    }
+},100);
+
+} catch (error) {
+     console.log(error);
+}
+
+setTimeout(() => {
+console.log("sushii pasword is hacking");
+},1000);
+
+setTimeout(() => {
+    console.log("sushii pasword is hacked");
+},2000);
+
+setTimeout(() => {
+console.log("sushii pasword will be hacking");
+},3000);
+
+setTimeout(() => {
+console.log("sushii pasword has been hacking");
+},4000);
+
+setTimeout(() => {
+console.log("sushii pasword hack vayoooo!");
+},5000);
